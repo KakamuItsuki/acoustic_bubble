3be98ba685a347306279ac2bc4b5d2d5676fa319
KakamuItsuki <84170149+KakamuItsuki@users.noreply.github.com>
2023-01-31 02:26:36 +0900

    correct commit

--- acoustic_bubble_all.f90
+++ acoustic_bubble_all.f90
@@ -1,15 +1,18 @@
 module globals
 	implicit none	
-	integer,parameter :: N=2, f=100000, n1 = 500, fooo = 15, n_max=10, foooo = 16, fooooo = 17, filesum = 5000
-	real (8) t, y(N), C(n1), n_mol, n_dot,  sum_alpha2, R_max, R(n1), n_mol_temp1, n_mol_temp2, r_temp1, r_temp2
+	integer,parameter :: N=2, f=100000, n1 = 500,  filesum = 5000,  n_max=10
+	real (8) t, y(N), C(n1), n_mol, n_dot,  sum_alpha2, R(n1)
 	real (8) Pg_dot, a_n(2*n_max), gamma, T_K(n1), V, RT(n1), U_dot, U, pre_y(N)
-	real(8), parameter :: dt=1.0d0*10.0d0**(-11.0d0) , Pi=3.141592650d0, Rg=8.3d0, T_K0=293.0d0, rho=1000.0d0
-	real(8), parameter :: P0=100000.0d0, nu=1.0d0*10.0d0**(-6.0d0), D=1.44d0*10.0d0**(-9.0d0), gamma0=0.07275d0
-	real(8), parameter :: H=0.0003d0, P_mugen=250000.0d0, Pv0=249990.0d0, A=1.2d0, dx=1.0d0*10.0d0**(-8), alpha = Pi
+	real(8), parameter :: dt=1.0d0*10.0d0**(-11.0d0) , Pi=3.141592650d0, Rg=8.3d0
+	real(8), parameter :: P0=100000.0d0, nu=1.0d0*10.0d0**(-6.0d0)
+	real(8), parameter :: D=1.4d0*10.0d0**(-9.0d0), A=1.2d0, rho=1000.0d0
+	real(8), parameter :: H=0.0003d0, P_mugen=250000.0d0, Pv0=249990.0d0
+	real(8), parameter :: gamma0=0.07275d0, dx=1.0d0*10.0d0**(-8), alpha = Pi
 	real(8), parameter :: wc = 1500.0d0, CO2 = 44.0d0, dr=1.0d0*10.0d0**(-10.0d0)
 	real(8), parameter :: br0 = 2 * gamma0 / (Pv0 - P0)* 1.0d0, Cv=29.0d0
-	real(8), parameter :: D_T=2.6d0*10.0d0**(-5.0d0), Tdx=3.0d0*10.0d0**(-7.0d0), D_T2=2.6d0*10.0d0**(-5.0d0)
+	real(8), parameter :: D_T=2.6d0*10.0d0**(-5.0d0), Tdx=3.0d0*10.0d0**(-7.0d0)
 	real(8), parameter :: t_max=1.0d0/1000*10, tstart=0.0d0/f, t2start=30.0d0/f
+	real(8), parameter :: D_T2=2.6d0*10.0d0**(-5.0d0), T_K0=293.0d0
 end module globals
 
 module subfunctions
@@ -29,16 +32,15 @@ module subfunctions
 		V = 4.0d0 * Pi * y1(1)**3 / 3
 
 		f1(1)= y1(2)
-		f1(2)= e1 * n_mol1 * T_K1(1) / V / y1(1) - e2 * (1 + A * sin(2 * Pi * f * t1 + alpha)) / y1(1) &
-			- 3 * y1(2)**2 / 2 / y1(1) - e3 * y1(2) / y1(1)**2 - e4 / y1(1)**2 
+		f1(2)= e1 * n_mol1 * T_K1(1) / V / y1(1) - e2 * (1 + A * sin(2 * Pi * f * t1 + alpha))&
+		 / y1(1) - 3 * y1(2)**2 / 2 / y1(1) - e3 * y1(2) / y1(1)**2 - e4 / y1(1)**2 
 	 	!y1(1)=radius  y1(2)=velocity  f(1)=rdot  f1(2)=vdot​
 
 	end function RP_func
 
 	function dif_func(y1,n_mol1,C1,R1,T_K1) result (f1)
-		!real(8), parameter :: 
-		real(8) t1, y1(N), f1(n1*2+1), C1(n1), C_plus_dt(n1), C_plus_dt2(n1), R1(n1), R_plus_dt(n1), J, n_dot1, n_mol1, alpha2
-		real(8) dR(n1), u_R(n1), T_K1(n1)
+		real(8) t1, y1(N), f1(n1*2+1), C1(n1), C_plus_dt(n1), C_plus_dt2(n1), R1(n1)
+		real(8) dR(n1), u_R(n1), T_K1(n1), R_plus_dt(n1), J, n_dot1, n_mol1, alpha2
 		integer i
 		
 		C_plus_dt(1) = H * 3 * n_mol1 * Rg * T_K(1) / 4 / Pi / y1(1)**3
@@ -55,9 +57,11 @@ module subfunctions
 		
 		
 		do i = 2, n1-1
-			C_plus_dt2(i) = C_plus_dt(i) + 2 * D / R_plus_dt(i) * (C_plus_dt(i) - C_plus_dt(i-1)) / (R_plus_dt(i) - R_plus_dt(i-1)) * dt &
-						+ D * ((C_plus_dt(i+1) - C_plus_dt(i)) / (R_plus_dt(i+1) - R_plus_dt(i)) - &
-						(C_plus_dt(i) - C_plus_dt(i-1)) / (R_plus_dt(i) - R_plus_dt(i-1))) / (R_plus_dt(i) - R_plus_dt(i-1)) * dt
+			C_plus_dt2(i) = C_plus_dt(i) + 2 * D / R_plus_dt(i) * (C_plus_dt(i) - &
+			C_plus_dt(i-1)) / (R_plus_dt(i) - R_plus_dt(i-1)) * dt + &
+			D * ((C_plus_dt(i+1) - C_plus_dt(i)) / (R_plus_dt(i+1) - R_plus_dt(i)) - &
+			(C_plus_dt(i) - C_plus_dt(i-1)) / (R_plus_dt(i) - R_plus_dt(i-1))) / &
+			(R_plus_dt(i) - R_plus_dt(i-1)) * dt
 		end do
 
 		J = D * (C_plus_dt2(2) - C_plus_dt2(1)) /  (R_plus_dt(2) - R_plus_dt(1))
@@ -76,8 +80,8 @@ module subfunctions
 
 	function temperature_func(y1,RT1,T_K1) result (f1)
 		!real(8), parameter :: 
-		real(8) t1, y1(N), f1(2*n1+1), RT1(n1), u_R(n1)
-		real(8) T_K1(n1), T_plus_dt(n1), T_plus_dt2(n1), R_plus_dt(n1), J, U_dot1
+		real(8) t1, y1(N), f1(2*n1+1), RT1(n1), u_R(n1), T_K1(n1)
+		real(8) T_plus_dt(n1), T_plus_dt2(n1), R_plus_dt(n1), J, U_dot1
 		integer i
 
 		T_plus_dt(1) = T_K1(1)
@@ -94,9 +98,11 @@ module subfunctions
 		end do
 
 		do i = 2, n1-1
-			T_plus_dt2(i) = T_plus_dt(i) + 2 * D_T / R_plus_dt(i) * (T_plus_dt(i) - T_plus_dt(i-1)) / (R_plus_dt(i) - R_plus_dt(i-1)) * dt &
-						+ D_T * ((T_plus_dt(i+1) - T_plus_dt(i)) / (R_plus_dt(i+1) - R_plus_dt(i)) - &
-						(T_plus_dt(i) - T_plus_dt(i-1)) / (R_plus_dt(i) - R_plus_dt(i-1))) / (R_plus_dt(i) - R_plus_dt(i-1)) * dt
+			T_plus_dt2(i) = T_plus_dt(i) + 2 * D_T / R_plus_dt(i) * (T_plus_dt(i) - &
+			T_plus_dt(i-1)) / (R_plus_dt(i) - R_plus_dt(i-1)) * dt + D_T * &
+			((T_plus_dt(i+1) - T_plus_dt(i)) / (R_plus_dt(i+1) - R_plus_dt(i)) - &
+			(T_plus_dt(i) - T_plus_dt(i-1)) / (R_plus_dt(i) - R_plus_dt(i-1))) / &
+			 (R_plus_dt(i) - R_plus_dt(i-1)) * dt
 		end do
 
 		J = D_T2 * (T_plus_dt2(2) - T_plus_dt2(1)) /  (R_plus_dt(2) - R_plus_dt(1))
@@ -115,8 +121,8 @@ module subfunctions
 	end function temperature_func
 
 	function spherical_harmonics(y1,a_n1,t1,n_mol1,Pg_dot1,T_K1) result (f1)
-		real(8) f1(2*n_max), y1(2), delta, a_n1(2*n_max), r2dot(2), t1, n_mol1, Pg_dot1, rhog, Anchil, Bnchil, An, Bn
-		real(8) T_K1(n1)
+		real(8) f1(2*n_max), y1(2), delta, a_n1(2*n_max), r2dot(2)
+		real(8) T_K1(n1), t1, n_mol1, Pg_dot1, rhog, Anchil, Bnchil, An, Bn
 		integer i
 		
 		do i = 1, n_max
@@ -124,10 +130,12 @@ module subfunctions
 			r2dot = RP_func(t1,y1,n_mol1,Pg_dot1,T_K1)
 			f1(2*i - 1) = a_n1(2*i)
 			rhog = 3 * n_mol * CO2 / 1000 / 4 / Pi / y1(1)**3
-			Anchil = (i-1)*(r2dot(2)/y1(1) - (i+1)*(i+2)*gamma/rho/y1(1)**3 - 2*nu*y1(2)/y1(1)**3 * &
-					((i+1)*(i+2) - i*(i+2)/(1+2*delta/y1(1))))
-			Bnchil = 3 * y1(2) / y1(1) + 2 * nu / y1(1)**2 * (-(i-1)*(i+1)*(i+2) + i*(i+2)**2 / (1+2*delta/y1(1)))
-			An =  rho/(i+1) / (rhog/i + rho/(i+1)) * Anchil - rhog/i / (rhog/i + rho/(i+1)) * (i-1) * y1(2)**2 / y1(1)
+			Anchil = (i-1)*(r2dot(2)/y1(1) - (i+1)*(i+2)*gamma/rho/y1(1)**3 - &
+			2*nu*y1(2)/y1(1)**3 * ((i+1)*(i+2) - i*(i+2)/(1+2*delta/y1(1))))
+			Bnchil = 3 * y1(2) / y1(1) + 2 * nu / y1(1)**2 * (-(i-1)*(i+1)*(i+2) + &
+			i*(i+2)**2 / (1+2*delta/y1(1)))
+			An =  rho/(i+1) / (rhog/i + rho/(i+1)) * Anchil - rhog/i / &
+			(rhog/i + rho/(i+1)) * (i-1) * y1(2)**2 / y1(1)
 			Bn = rho/(i+1) / (rhog/i + rho/(i+1)) * Bnchil
 			f1(2*i) =  -1.0d0 * Bn * a_n1(2*i) + An * a_n1(2*i-1)
 		end do
@@ -156,10 +164,14 @@ module subroutines
 	subroutine Runge_Kutta_SH
 		real(8) k1(2*n_max),k2(2*n_max),k3(2*n_max),k4(2*n_max)
 		k1(1:2*n_max)=spherical_harmonics(y(1:N),a_n(1:2*n_max),t,n_mol,Pg_dot,T_K)
-		k2(1:2*n_max)=spherical_harmonics(y(1:N), a_n(1:2*n_max)+1/2*dt*k1(1:2*n_max), t,n_mol,Pg_dot,T_K)
-		k3(1:2*n_max)=spherical_harmonics(y(1:N), a_n(1:2*n_max)+1/2*dt*k2(1:2*n_max), t,n_mol,Pg_dot,T_K)
-		k4(1:2*n_max)=spherical_harmonics(y(1:N), a_n(1:2*n_max)+dt*k3(1:2*n_max), t,n_mol,Pg_dot,T_K)
-		a_n(1:2*n_max) = a_n(1:2*n_max) + dt*( k1(1:2*n_max) + 2*k2(1:2*n_max) + 2*k3(1:2*n_max) + k4(1:2*n_max) )/6
+		k2(1:2*n_max)=spherical_harmonics(y(1:N), a_n(1:2*n_max)+&
+		1/2*dt*k1(1:2*n_max), t,n_mol,Pg_dot,T_K)
+		k3(1:2*n_max)=spherical_harmonics(y(1:N), a_n(1:2*n_max)+&
+		1/2*dt*k2(1:2*n_max), t,n_mol,Pg_dot,T_K)
+		k4(1:2*n_max)=spherical_harmonics(y(1:N), a_n(1:2*n_max)+&
+		dt*k3(1:2*n_max), t,n_mol,Pg_dot,T_K)
+		a_n(1:2*n_max) = a_n(1:2*n_max) + dt*( k1(1:2*n_max) + &
+		2*k2(1:2*n_max) + 2*k3(1:2*n_max) + k4(1:2*n_max) )/6
 	end subroutine
 
 	subroutine Euler_dif
@@ -189,7 +201,8 @@ module subroutines
 			RT(i) = f(i+n1+1)
 		end do
 
-		U = U + dt * U_dot - n_mol * Rg * T_K(1) / (4.0d0 * Pi * y(1)**3 / 3) * (4.0d0 * Pi / 3) * (y(1)**3 - pre_y(1)**3)
+		U = U + dt * U_dot - n_mol * Rg * T_K(1) / (4.0d0 * Pi * y(1)**3 / 3) * &
+		(4.0d0 * Pi / 3) * (y(1)**3 - pre_y(1)**3)
 
 		T_K(1) = U / Cv / n_mol
 	end subroutine
@@ -201,10 +214,10 @@ program RK
 	use subfunctions
 	implicit none
 	integer(8) i, imax, j, k, filenum, orbitnum
-	integer,parameter :: fo=11, foo=13, jmax=20, kmax=60
+	integer,parameter :: jmax=20, kmax=60
 	real(8),parameter::  xrange=5.0d0, yrange=10.0d0*10.0d0**(-6.0d0)
-	real(8) a2dot, SH(2*n_max), g1, g2, g3, g4, g5, x_vec, y_vec, xx, yy, v_plus, v_minus, rr
-	real(8) orbit_r(500), orbit_v(500),TF(2*n1+1),Pv, PdV
+	real(8) g1, g2, g3, g4, g5, x_vec, y_vec, xx, yy, v_plus, v_minus, rr
+	real(8) orbit_r(500), orbit_v(500),TF(2*n1+1),Pv, PdV, a2dot, SH(2*n_max)
 	character(40) ffout,fffout, ffffout, fout
 
 	filenum = 0
@@ -243,31 +256,31 @@ program RK
 
 		if (mod(i, int(imax/0.5/10**3)) == 1)then
 			write(fout,'("DCR2_",i4.4,".d")') i/int(imax/0.5/10**3)
-			open(foo,file=fout)
+			open(12,file=fout)
 			do j = 1, n1-2
-				write(foo,*) R(j), C(j), sin(2 * Pi * f * t + alpha)
+				write(12,*) R(j), C(j), sin(2 * Pi * f * t + alpha)
 			end do
 			! write(fout,'("DCT2_",i4.4,".d")') i/int(imax/0.5/10**3)
-			! open(foo,file=fout)
+			! open(13,file=fout)
 			! do j = 1, n1
-			! 	write(foo,*) RT(j), T_K(j), sin(2 * Pi * f * t + alpha)
+			! 	write(13,*) RT(j), T_K(j), sin(2 * Pi * f * t + alpha)
 			! end do
 		end if
 		if (mod(i,int((imax-int(tstart/dt))/filesum/10)) == 1 .and. t>tstart)then
 			Pv = n_mol * Rg * T_K(1) / (4.0d0 / 3 * Pi * y(1)**3)
 			PdV = Pv * (4.0d0 * Pi / 3) * (y(1)**3 - pre_y(1)**3)
-			!write(fo,*) t, y(1),y(2),T_K(1),U_dot!n_dot/4/Pi/y(1)**2!, RP_func(t,y,n_mol,Pg_dot)!a_n!y(1),y(2),sin(2 * Pi * f * t + alpha)
-			!write(fo,*)t,Pv,4.0d0 / 3 * Pi * y(1)**3,T_K(1), Pv*4.0d0 / 3 * Pi * y(1)**3,n_mol,y(1),U_dot,U_dot*dt,PdV,y(1)**3 - pre_y(1)**3
-			write(fo,*)t,y(1),y(2),T_K(1),n_mol
+			write(11,*)t,y(1),y(2),T_K(1),n_mol
 			if(t>tstart)then
 				write(ffout,'("nullcline2_",i4.4,".d")') filenum
 				write(fffout,'("nullcline2_orbit_",i4.4,".d")')filenum
 				write(ffffout,'("nullcline2_vector_",i4.4,".d")')filenum
-				open(fooo,file=ffout)
-				open(foooo,file=fffout)
-				open(fooooo,file=ffffout)
-				a2dot = -1.0d0 / y(1) * (3 * y(2) + 18 * nu / (y(1) + 2 * min((nu / (2 * Pi * f))**(1.0d0/2), y(1) / 2)))
-				SH(1:2*n_max) = spherical_harmonics(y(1:N),a_n(1:2*n_max),t,n_mol,Pg_dot,T_K)
+				open(14,file=ffout)
+				open(15,file=fffout)
+				open(16,file=ffffout)
+				a2dot = -1.0d0 / y(1) * (3 * y(2) + 18 * nu / (y(1) + &
+				2 * min((nu / (2 * Pi * f))**(1.0d0/2), y(1) / 2)))
+				SH(1:2*n_max) = spherical_harmonics(y(1:N),a_n(1:2*n_max),&
+				t,n_mol,Pg_dot,T_K)
 				
 				g1 = 3 * Rg * T_K(1) / 4 / rho / Pi
 				g2 = P0 / rho
@@ -279,27 +292,34 @@ program RK
 					do k = 0, kmax
 						xx = xrange * j / jmax
 						yy = yrange * k / kmax
-						x_vec = g1 * n_mol / yy**4 - g2 * (1 + A * sin(2 * Pi * f * t + alpha)) / yy &
-							- 3 * xx**2 / 2 / yy - g3 * xx / yy**2 - g4 / yy**2
+						x_vec = g1 * n_mol / yy**4 - g2 * (1 + A * &
+						sin(2 * Pi * f * t + alpha)) / yy - 3 * xx**2 / 2 &
+						/ yy - g3 * xx / yy**2 - g4 / yy**2
 						y_vec = xx
-						write(fooooo,*)xx, yy*10**6, x_vec*3*10.0d0**(-9.0d0), y_vec*10.0d0**(-7.0d0)*10**6
+						write(16,*)xx, yy*10**6, x_vec*3*10.0d0**(-9.0d0), &
+						y_vec*10.0d0**(-7.0d0)*10**6
 					end do
 				end do
 				orbit_r(orbitnum) = y(1)
 				orbit_v(orbitnum) = y(2)
 				do j = 10000, 40000
 					rr = j * dr
-					v_plus = - 4 * nu / 3 / rr + (16 * nu**2 / 9 / rr**2 + Rg * T_K(1) / 2 / rho / Pi * n_mol / rr**3 &
-						- 2 * P0 / 3 / rho * (1 + A * sin(2 * Pi * f * t + alpha)) -  4 * gamma0 / 3 / rho / rr)**(1.0d0/2)
-					v_minus = - 4 * nu / 3 / rr - (16 * nu**2 / 9 / rr**2 + Rg * T_K(1) / 2 / rho / Pi * n_mol / rr**3 &
-						- 2 * P0 / 3 / rho * (1 + A * sin(2 * Pi * f * t + alpha)) -  4 * gamma0 / 3 / rho / rr)**(1.0d0/2)
-					write(fooo,*)v_plus, v_minus, rr*10**6, y(2), y(1)**10**6, xrange*10**6/30
+					v_plus = - 4 * nu / 3 / rr + (16 * nu**2 / 9 / rr**2 + Rg * &
+					T_K(1) / 2 / rho / Pi * n_mol / rr**3 &
+					- 2 * P0 / 3 / rho * (1 + A * sin(2 * Pi * f * t + alpha)) - &
+					 4 * gamma0 / 3 / rho / rr)**(1.0d0/2)
+					v_minus = - 4 * nu / 3 / rr - (16 * nu**2 / 9 / rr**2 + Rg * &
+					T_K(1) / 2 / rho / Pi * n_mol / rr**3 &
+					- 2 * P0 / 3 / rho * (1 + A * sin(2 * Pi * f * t + alpha)) - &
+					 4 * gamma0 / 3 / rho / rr)**(1.0d0/2)
+					write(14,*)v_plus, v_minus, rr*10**6, y(2), y(1)**10**6, &
+					xrange*10**6/30
 				end do
 				filenum = filenum + 1
 				orbitnum = orbitnum + 1
 
 				do j = 1, orbitnum-1
-					write(foooo,*)orbit_r(j)*10**6, orbit_v(j)
+					write(15,*)orbit_r(j)*10**6, orbit_v(j)
 				end do
 					
 
@@ -325,9 +345,6 @@ program RK
     
 	end do
 	write(*,*)t*f,'周期',t,'秒'
-	close(fo)
+	close(11)
 	
 end program RK
-
-
-
